a1a2ece5bae1422c7dab02f647a18bda6a9ed988
EvaGraceSmith <smithevagrace@gmail.com>
2023-01-08 23:40:10 -0600

    added stretch goal

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import React from "react";
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import {beast} from './Beast.js';
+
 
 class App extends React.Component {
   render(){
     return(
     <div>
       <Header/>
-<h1> Hi</h1>
-<p>this a paragraph</p>
+
+      {
+beast.map(beast => (
+  <><h2> {beast.title}</h2><img src={beast.image_url} alt="animal" title="title" /><p>{beast.description}</p></>
+))
+}
+      
 <Main title='Unicorn' description= 'white and fluffy'/>
 <Main title='Rhino' description= 'grey and angry'/>
+
 <Footer/>
 </div>
     )
