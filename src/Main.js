54fac6d2d42e9c12e78068ef9e3cc74b44809a1a
EvaGraceSmith <smithevagrace@gmail.com>
2023-01-10 20:20:00 -0600

    trouble shooting, added .json

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js'
-import { beastsData } from './Beast.js';
+import beastsData from './Beast.json';
 
 class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.setState(beastsData);
-    }
+    // constructor(props) {
+    //     super(props);
+    //     // this.setState(beastsData);
+    // }
     render() {
+        console.log(beastsData);
         return (
             <>
-            <HornedBeast title={this.props.title} image_url={this.props.image_url} description={this.props.description} />
+            {/* <HornedBeast title={this.props.title} image_url={this.props.image_url} description={this.props.description} /> */}
+                   {/* {
+          beastsData.map(beast => (
+            <HornedBeast title={beast.title} image_url={beast.image_url} description={beast.description} />
+          ))
+        } */}
             </>
         )
 }
